fbfba284cab0f6b766f106ffa0572286090eebb5
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2018-03-25 10:55:38 -0400

    Create solution.cpp

---
+++ Cpp/Say_Hello_World_with_Cpp/solution.cpp
@@ -0,0 +1,13 @@
+/*
+* Author: greeneyedgeek
+* Date: 2018-03-25
+*/
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
