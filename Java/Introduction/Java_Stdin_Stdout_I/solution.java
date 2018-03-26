12fb3f75bb79a21da1ab505702563500eb45f965
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2018-03-26 00:57:54 -0400

    Create solution.java

---
+++ Java/Introduction/Java_Stdin_Stdout_I/solution.java
@@ -0,0 +1,24 @@
+/*
+* Author: greeneyedgeek
+* Date: 2018-03-26
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
