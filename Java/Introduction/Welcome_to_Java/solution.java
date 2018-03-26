2c78f995fa3ddf55a53d664f75d1b4438c4a5218
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2018-03-25 23:07:15 -0400

    Create solution.java

---
+++ Java/Introduction/Welcome_to_Java/solution.java
@@ -0,0 +1,13 @@
+/*
+* Author: greeneyedgeek
+* Date: 2018-03-25
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
