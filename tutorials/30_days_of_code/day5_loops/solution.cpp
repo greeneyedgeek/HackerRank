574218d7c30b4b612b5fe20adce36ce8b5e1d8f5
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2018-03-30 12:17:50 -0400

    Create solution.cpp

---
+++ tutorials/30_days_of_code/day5_loops/solution.cpp
@@ -0,0 +1,19 @@
+/*
+* Author: greeneyedgeek
+* Date: 2018-03-30
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    for (int i = 1; i <= 10; i++) {
+        
+        cout << n << " x " << i << " = " << n*i << endl;
+        
+    }
+    return 0;
+}
