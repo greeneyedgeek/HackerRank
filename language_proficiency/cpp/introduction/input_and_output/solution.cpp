eda879229a0b2ff0b28a7d73ee31306ab53ee921
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2018-04-01 00:17:14 -0400

    Create solution.cpp

---
+++ language_proficiency/cpp/introduction/input_and_output/solution.cpp
@@ -0,0 +1,23 @@
+/*
+* Author: greeneyedgeek
+* Date: 2018-04-01
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int sum;
+    int input;
+    
+    while (cin >> input) {
+        
+        sum += input;
+        
+    }
+    
+    cout << sum;
+    
+    return 0;
+}
