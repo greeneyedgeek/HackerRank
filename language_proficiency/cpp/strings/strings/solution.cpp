7fc38a1094799cdcb33fe13ca11f24c329ead84e
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2019-02-14 08:19:33 -0500

    Create solution.cpp

---
+++ language_proficiency/cpp/strings/strings/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// Complete the program
+    string a, b;
+    cin >> a >> b;
+    cout << a.length()<< ' ' << b.length() << endl;
+    cout << a << b << endl;
+
+    char temp = a.at(0);
+    a.at(0) = b.at(0);
+    b.at(0) = temp;
+    cout << a << ' ' << b << endl;
+
+    return 0;
+}
+
