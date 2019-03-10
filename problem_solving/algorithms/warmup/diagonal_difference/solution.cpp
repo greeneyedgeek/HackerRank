cf7545804d49d382d9b37f7cc231d8829f4d0c3e
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2019-03-09 20:09:11 -0500

    Create solution.cpp

---
+++ problem_solving/algorithms/warmup/diagonal_difference/solution.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the diagonalDifference function below.
+int diagonalDifference(vector<vector<int>> arr) {
+    int primary{0};
+    int secondary{0};
+    for (int i = 0; i < arr.size(); i++) {
+        primary += arr[i][i];
+        secondary += arr[i][(arr.size()-1)-i];
+    }
+    return primary > secondary ? primary - secondary : secondary - primary;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<int>> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
