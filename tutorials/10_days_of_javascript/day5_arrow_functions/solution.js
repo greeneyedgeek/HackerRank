e8cf8c8c94e8f1f05ede6befe6ba785062738c03
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2019-03-16 15:56:40 -0400

    Create solution.js

---
+++ tutorials/10_days_of_javascript/day5_arrow_functions/solution.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    let modify = n => (n % 2 === 0) ? 2 * n : 3 * n;
+    let mod_nums = nums.map(modify);
+    return mod_nums;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
