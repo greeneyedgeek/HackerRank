ca1eb232b29bd7ffeb83829164f9d463127f380f
greeneyedgeek <35167787+greeneyedgeek@users.noreply.github.com>
2019-03-17 22:03:46 -0400

    Create solution.c

---
+++ language_proficiency/c/introduction/sum_and_difference/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, m;
+    float x, y;
+
+    scanf("%d %d", &n, &m);
+    scanf("%f %f", &x, &y);
+
+    printf("%d %d\n", n + m, n - m);
+    printf("%.1f %.1f", x + y, x - y);
+
+    return 0;
+}
+
